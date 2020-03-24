5fd19ed5ea0ef866e6c7c90fd3a556bb82b69f2c
ar-erik.blanca <erik.bg7@gmail.com>
2020-03-24 20:46:40 +0100

    Remove data mocks

--- lib/tfg/activityList.dart
+++ lib/tfg/activityList.dart
@@ -5,36 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ActivityList extends StatelessWidget {
   ActivityList({Key key}) : super(key: key);
-
-  final List<Object> objects = [
-    {
-      "name": "AIGUESTORTES",
-      "image": "Aiguestortes.jpg",
-      "descr": "El Parc Nacional"
-    },
-    {"name": "LES FALLES", "image": "falles.jpg", "descr": "La Festa del Foc"},
-    {"name": "El BALNEARI", "image": "balneari.jpg", "descr": "Caldes de Boí"},
-    {
-      "name": "EL ROMANIC",
-      "image": "romanic.jpg",
-      "descr": "Patrimoni Cultural"
-    },
-    {"name": "LES PISTES", "image": "esqui.jpeg", "descr": "Boí Taüll Resort"},
-    {
-      "name": "AIGUESTORTES",
-      "image": "Aiguestortes.jpg",
-      "descr": "El Parc Nacional"
-    },
-    {"name": "LES FALLES", "image": "falles.jpg", "descr": "La Festa del Foc"},
-    {"name": "El BALNEARI", "image": "balneari.jpg", "descr": "Caldes de Boí"},
-    {
-      "name": "EL ROMANIC",
-      "image": "romanic.jpg",
-      "descr": "Patrimoni Cultural"
-    },
-    {"name": "LES PISTES", "image": "esqui.jpeg", "descr": "Boí Taüll Resort"},
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
